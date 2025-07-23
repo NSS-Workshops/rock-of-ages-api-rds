ea999d68119e8d97b6fe01c1f2f96906c0db723b
Valerie Freeman <valerah7@gmail.com>
2025-07-23 11:26:46 -0500

    docker environment setup

--- rockproject/settings.py
+++ rockproject/settings.py
@@ -99,7 +99,7 @@ DATABASES = {
         'HOST': os.environ.get('DB_HOST'),
         'PORT': os.environ.get('DB_PORT', '5432'),
         'OPTIONS': {
-            'sslmode': 'require',
+            'sslmode': os.environ.get('SSLMODE', 'require'),
         },
     }
 }
